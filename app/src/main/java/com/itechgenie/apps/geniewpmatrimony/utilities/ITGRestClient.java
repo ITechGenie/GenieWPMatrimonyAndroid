31ef8acbf3fb53340af87d12244630ba5a94c093
PRAKASH-HP-LAP\Prakash <prakashm88@yahoo.co.in>
2017-04-28 04:19:25 +0530

    new updates

---
+++ app/src/main/java/com/itechgenie/apps/geniewpmatrimony/utilities/ITGRestClient.java
@@ -0,0 +1,8 @@
+package com.itechgenie.apps.geniewpmatrimony.utilities;
+
+/**
+ * Created by Prakash-hp on 15-04-2017.
+ */
+
+public class ITGRestClient {
+}
---
+++ app/src/main/java/com/itechgenie/apps/geniewpmatrimony/utilities/OAuthUtil.java
@@ -0,0 +1,8 @@
+package com.itechgenie.apps.geniewpmatrimony.utilities;
+
+/**
+ * Created by Prakash-hp on 23-04-2017.
+ */
+
+public class OAuthUtil {
+}
